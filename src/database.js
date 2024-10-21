084795e813a28f96a32552a5afef2869e5fb8601
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-22 04:00:48 +0500

    feat: replace mock data with the classes to create objects and keep track of the data

---
+++ src/database.js
@@ -0,0 +1,49 @@
+class User {
+  constructor(id, username, age, hobbies) {
+    this.id = id;
+    this.username = username;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
+
+  update(username, age, hobbies) {
+    this.username = username;
+    this.age = age;
+    this.hobbies = hobbies;
+    return this;
+  }
+}
+
+class Database {
+  #users;
+
+  constructor() {
+    this.#users = [];
+  }
+
+  createUser(id, username, age, hobbies) {
+    const user = new User(id, username, age, hobbies);
+    this.#users.push(user);
+    return user;
+  }
+
+  getUsers() {
+    return this.#users;
+  }
+
+  getUser(userId) {
+    return this.#users.find(user => user.id === userId);
+  }
+
+  removeUser(userIndex) {
+    this.#users.splice(userIndex, 1)[0]
+  }
+
+  updateUser(userId, username, age, hobbies) {
+    return this.#users
+      .find(user => user.id === userId)
+      .update(username, age, hobbies);
+  }
+}
+
+export default Database;
